be5ef74a6a674a2fbbf86cbcbd5352ea033feee7
vanessaruama <vanessaruama@gmail.com>
2025-06-25 16:59:46 -0300

    mudando para o método getByTestId que usa a tag data-testid

--- e2e/visitarPaginaInicial.spec.ts
+++ e2e/visitarPaginaInicial.spec.ts
@@ -3,16 +3,19 @@ import { test, expect } from "@playwright/test";
 test.describe("Página Inicial", () => {
   test("Deve visitar a página inicial", async ({page}) => {
     //sempre utilizar o async/await para ações assíncronas
-    await page.goto("http://localhost:4200"); //ação de navegação
+    await page.goto("/"); //ação de navegação
     await expect(page).toHaveTitle("Jornada Milhas"); //asserção
 
-    const tituloPassagens = page.getByRole("heading", { name: "Passagens" });
+    // const tituloPassagens = page.getByRole("heading", { name: "Passagens" }); //getByRole é usado para selecionar elementos com base no seu papel (role) e nome
+    const tituloPassagens = page.getByTestId('titulo-passagens'); //getByTestId é usado para selecionar elementos com base no atributo data-testid
     await expect(tituloPassagens).toBeVisible(); //verifica se o título está visível
 
-    const tituloPromocoes = page.getByRole("heading", { name: "Promoções" });
-    await expect(tituloPromocoes).toBeVisible(); //verifica se o título está visível
+    // const tituloPromocoes = page.getByRole("heading", { name: "Promoções" });
+    const tituloPromocoes = page.getByTestId('titulo-promocoes');
+    await expect(tituloPromocoes).toBeVisible();
 
-    const tituloDepoimentos = page.getByRole("heading", { name: "Depoimentos" });
-    await expect(tituloDepoimentos).toBeVisible(); //verifica se o título está visível
+    // const tituloDepoimentos = page.getByRole("heading", { name: "Depoimentos" });
+    const tituloDepoimentos = page.getByTestId('titulo-depoimentos');
+    await expect(tituloDepoimentos).toBeVisible();
   });
 });
