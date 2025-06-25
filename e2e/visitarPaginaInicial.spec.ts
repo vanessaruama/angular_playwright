b7a2836b55f065df0b5a7e88db6226a90b26b0b7
vanessaruama <vanessaruama@gmail.com>
2025-06-25 16:50:40 -0300

    usando o getByRole

--- e2e/visitarPaginaInicial.spec.ts
+++ e2e/visitarPaginaInicial.spec.ts
@@ -2,7 +2,17 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Página Inicial", () => {
   test("Deve visitar a página inicial", async ({page}) => {
-    await page.goto("http://localhost:4200");
-    await expect(page).toHaveTitle("Jornada Milhas");
+    //sempre utilizar o async/await para ações assíncronas
+    await page.goto("http://localhost:4200"); //ação de navegação
+    await expect(page).toHaveTitle("Jornada Milhas"); //asserção
+
+    const tituloPassagens = page.getByRole("heading", { name: "Passagens" });
+    await expect(tituloPassagens).toBeVisible(); //verifica se o título está visível
+
+    const tituloPromocoes = page.getByRole("heading", { name: "Promoções" });
+    await expect(tituloPromocoes).toBeVisible(); //verifica se o título está visível
+
+    const tituloDepoimentos = page.getByRole("heading", { name: "Depoimentos" });
+    await expect(tituloDepoimentos).toBeVisible(); //verifica se o título está visível
   });
 });
