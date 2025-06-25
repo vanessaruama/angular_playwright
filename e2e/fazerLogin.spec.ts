f8f91d83bd4fe428b7ea906ffad08286fea34d26
vanessaruama <vanessaruama@gmail.com>
2025-06-25 17:23:27 -0300

    teste do login

--- e2e/fazerLogin.spec.ts
+++ e2e/fazerLogin.spec.ts
@@ -4,6 +4,9 @@ import PaginaLogin from "./page-objects/PaginaLogin";
 test.describe("Página de Login", () => {
   test("Deve conseguir fazer login com email e senha válidos", async ({ page }) => {
     const paginaLogin = new PaginaLogin(page);
+
     await paginaLogin.visitar();
+    await paginaLogin.fazerLogin("ruama@gmail.com", "1234");
+    await paginaLogin.loginFeitoComSucesso();
   });
 });
--- e2e/page-objects/PaginaLogin.ts
+++ e2e/page-objects/PaginaLogin.ts
@@ -1,13 +1,33 @@
-import { Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 
 export default class PaginaLogin {
   private readonly page: Page;
+  private readonly botaoLogin: Locator; //Elementos da tela
+  private readonly inputEmail: Locator;
+  private readonly inputSenha: Locator;
+  private readonly botaoAcessarConta: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.botaoLogin = page.getByTestId("botao-login");
+    this.inputEmail = page.getByTestId("input-email");
+    this.inputSenha = page.getByTestId("input-senha");
+    this.botaoAcessarConta = page.getByTestId("botao-acessar-conta");
   }
 
   async visitar() {
     await this.page.goto("/");
+    await this.botaoLogin.click(); //Clica no botão de login
+    await expect(this.page).toHaveURL("/auth/login"); //Verifica se a URL mudou para /auth/login
+  }
+
+  async fazerLogin(email: string, senha: string) {
+    await this.inputEmail.fill(email); //Preenche o campo de email
+    await this.inputSenha.fill(senha); //Preenche o campo de senha
+    await this.botaoAcessarConta.click(); //Clica no botão de acessar conta
+  }
+
+  async loginFeitoComSucesso() {
+    await expect(this.page).toHaveURL("/home"); //Verifica se a URL voltou para a página inicial
   }
 }
