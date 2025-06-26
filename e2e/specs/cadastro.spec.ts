613813d3d8dc12d240b77fbc680e73e73ed57e7c
vanessaruama <vanessaruama@gmail.com>
2025-06-25 22:10:15 -0300

    ajuste do timeout para o playwright

--- e2e/specs/cadastro.spec.ts
+++ e2e/specs/cadastro.spec.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from "@playwright/test";
 import { gerarPerfil, Perfil } from "e2e/operacoes/gerarPerfil";
 import { test } from "e2e/setup/fixtures";
-
-export default defineConfig({
-  timeout: 80000 //Define um tempo maior para o teste, caso necessário, aqui rodou em 55 segundos
-});
 
 test.describe("Página de Cadastro", () => {
   let novoUsuario: Perfil;
